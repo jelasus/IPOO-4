a071f11c8abbdbef67400538225f7d1a6f3c2a7e
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2016-11-26 17:55:24 -0500

    Add files via upload

---
+++ assigment4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template<typename T>
+T min(T val1,T val2){
+	if(val1<val2)
+		return val1;
+	else
+		return val2;
+}
+
+#define min2(x,y) (x < y ? x : y)
+
+static_cast <Triangle *> (pointer);
+dynamic_cast <Triangle *> (pointer);
